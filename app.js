d2fff8eef90e9a0194e529e1ad586a29d48c7fd7
Christian Moll <christian@chrmoll.de>
2018-02-24 23:01:44 +0100

    adding auth and routing in logged in and logged out state

--- app.js
+++ app.js
@@ -4,27 +4,45 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+var bcrypt = require('bcrypt');
+var config = require('nconf');
+config.file({ file: 'config/config.json' });
+
+const saltRounds = 10;
 
 let sess;
-const title = 'MediaPlayerPi';
+const title = config.get('title');
 
 app.set('views', './views');
 app.set('view engine', 'hjs');
 app.disable('x-powered-by');
 
+if (!config.get('admin:password')) {
+  bcrypt.hash('admin', saltRounds, function(err, hash) {
+    console.log(hash);
+  });
+}
+
 var readSession = function (req, res, next) {
   sess = req.session;
-  if (sess.email) sess.showEmail = true;
-  console.log(`Email: ${sess.email} Password: ${sess.password}`);
+  if (sess.user) {
+    sess.showUser = true;
+  } else {
+    if (req.path !== '/login' && req.path !== '/screen') {
+      return res.redirect('/login');
+    }
+  }
+  console.log(`User: ${sess.user} Password: ${sess.password}`);
   next();
 };
 
 //add some middleware
 
+app.use(express.static('static'));
 app.use(express.static('upload'));
 app.use(session({
   secret: 'sosecret',
-  name: 'MediaPlayerPi',
+  name: `${title}`,
   resave: true,
   saveUninitialized: true
 }));
@@ -33,24 +51,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(readSession);
 
 app.listen(3000, function () {
-  console.log('MediaPlayerPi listening on port 3000!');
+  console.log(`${title} listening on port 3000!`);
 });
 
 app.get('/', function (req, res) {
-  if (!sess.email) {
-    res.redirect('/login');
-  } else {
-    res.render('index', {
-      title: `${title}`,
-      showEmail: sess.showEmail,
-      email: `${sess.email}`
-    });
-  }
+  res.redirect('/admin');
+});
+
+app.get ('/admin', (req, res) => {
+  res.render('admin', {
+    title: `${title}`,
+    showUser: sess.showUser,
+    user: `${sess.user}`
+  });
 });
 
 app.get('/login', function (req, res) {
-  if (sess.email) {
-    res.redirect('/');
+  if (sess.user) {
+    res.redirect('/admin');
   } else {
     res.render('login', {
       title: `${title}`
@@ -59,9 +77,25 @@ app.get('/login', function (req, res) {
 });
 
 app.post('/login', (req, res) => {
-  sess.email=req.body.email;
-  sess.password=req.body.pass;
-  res.end('done');
+  console.log('check password');
+  if (req.body.user === config.get('admin:user')) {
+    let hash = config.get('admin:password');
+    bcrypt.compare(req.body.pass, hash, function(err, resp) {
+      if (resp === true) {
+        console.log('password correct');
+        sess.user=req.body.user;
+        sess.password=req.body.pass;
+        res.end('done');
+      } else {
+        res.end('wrong password');
+        console.log('password wrong');
+      }
+    });
+  } else {
+    res.end('wrong user');
+    console.log('username wrong');
+  }
+
 });
 
 app.get('/logout', (req,res) => {
@@ -82,10 +116,18 @@ app.get('/screen', function (req, res) {
       logo = "logo.png";
     }
   }
-  res.render('screensaver', {
+  res.render('screen', {
     title: `${title}`,
     logo: `${logo}`,
-    showEmail: sess.showEmail,
-    email: `${sess.email}`
+    showUser: sess.showUser,
+    user: `${sess.user}`
   });
 });
+
+function saveConfig() {
+  config.save(function (err) {
+    fs.readFile('config/config.json', function (err, data) {
+      console.dir(JSON.parse(data.toString()))
+    });
+  });
+}
--- run.sh
+++ run.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-
-# disable DPMS (Energy Star) features.
 xset -dpms
-
-# disable screen saver
 xset s off
-
-# don't blank the video device
 xset s noblank
-
-# disable mouse pointer
 unclutter &
-
-# run window manager
 matchbox-window-manager -use_cursor no -use_titlebar no  &
-
 cd ~/MediaPlayerPi/
 node app.js &
-# run browser
 midori -e Fullscreen -a http://localhost:3000/screen
